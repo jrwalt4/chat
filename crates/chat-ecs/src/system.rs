7ec538071a7daf940d78c05d6e5fd92ed61fe814
Reese Walton <jrwalt4@gmail.com>
2024-06-03 19:42:31 -0400

    Impl FunctionSystem
    Create an intermediate FunctionSystemDef that holds system information
    but needs to be initialized with a World.

--- crates/chat-ecs/src/system.rs
+++ crates/chat-ecs/src/system.rs
@@ -21,47 +21,48 @@ pub enum SystemError {
 
 pub trait SystemParam: Sized {
     type State: Send + Sync + 'static;
-    type Item<'world, 'state>: SystemParam<State = Self::State>;
 
-    unsafe fn get_item<'world, 'state>(
+    fn init_state(world: &World) -> Self::State;
+
+    unsafe fn from_state<'world, 'state>(
         state: &'state mut Self::State,
         world: &'world World,
-    ) -> Self::Item<'world, 'state>;
+    ) -> Self;
 }
 
-pub type SystemParamItem<'world, 'state, P> = <P as SystemParam>::Item<'world, 'state>;
+// pub type SystemParamItem<'world, 'state, P> = <P as SystemParam>::Item<'world, 'state>;
 
 impl<P0: SystemParam, P1: SystemParam> SystemParam for (P0, P1) {
     type State = (P0::State, P1::State);
-    type Item<'world, 'state> = (
-        SystemParamItem<'world, 'state, P0>,
-        SystemParamItem<'world, 'state, P1>,
-    );
-    unsafe fn get_item<'world, 'state>(
+
+    fn init_state(world: &World) -> Self::State {
+        (P0::init_state(world), P1::init_state(world))
+    }
+    unsafe fn from_state<'world, 'state>(
         state: &'state mut Self::State,
         world: &'world World,
-    ) -> Self::Item<'world, 'state> {
+    ) -> Self {
         let (state0, state1) = state;
-        (P0::get_item(state0, world), P1::get_item(state1, world))
+        (P0::from_state(state0, world), P1::from_state(state1, world))
     }
 }
 
 impl<P0: SystemParam, P1: SystemParam, P2: SystemParam> SystemParam for (P0, P1, P2) {
     type State = (P0::State, P1::State, P2::State);
-    type Item<'world, 'state> = (
-        SystemParamItem<'world, 'state, P0>,
-        SystemParamItem<'world, 'state, P1>,
-        SystemParamItem<'world, 'state, P2>,
-    );
-    unsafe fn get_item<'world, 'state>(
+
+    fn init_state(world: &World) -> Self::State {
+        (P0::init_state(world), P1::init_state(world), P2::init_state(world))
+    }
+
+    unsafe fn from_state<'world, 'state>(
         state: &'state mut Self::State,
         world: &'world World,
-    ) -> Self::Item<'world, 'state> {
+    ) -> Self {
         let (state0, state1, state2) = state;
         (
-            P0::get_item(state0, world),
-            P1::get_item(state1, world),
-            P2::get_item(state2, world),
+            P0::from_state(state0, world),
+            P1::from_state(state1, world),
+            P2::from_state(state2, world),
         )
     }
 }
@@ -71,19 +72,36 @@ pub trait SystemParamFunction<Hint>: Send + Sync + 'static {
     type Output;
     type Params: SystemParam;
 
-    fn run(&self, input: Self::Input, params: SystemParamItem<Self::Params>) -> Self::Output;
+    fn run(&self, input: Self::Input, params: Self::Params) -> Self::Output;
+}
+
+impl<Out, P0: SystemParam, F: Send + Sync + 'static> SystemParamFunction<fn(P0) -> Out>
+    for F
+where
+    for<'a> &'a F: Fn(P0) -> Out,
+{
+    type Input = ();
+    type Output = Out;
+    type Params = P0;
+
+    fn run(&self, _input: Self::Input, params: Self::Params) -> Self::Output {
+        fn call_inner<O, P>(f: impl Fn(P) -> O, p: P) -> O {
+            f(p)
+        }
+        call_inner(self, params)
+    }
 }
 
 impl<In, Out, P0: SystemParam, F: Send + Sync + 'static> SystemParamFunction<fn(In, P0) -> Out>
     for F
 where
-    for<'a> &'a F: Fn(In, SystemParamItem<'_, '_, P0>) -> Out,
+    for<'a> &'a F: Fn(In, P0) -> Out,
 {
     type Input = In;
     type Output = Out;
     type Params = P0;
 
-    fn run(&self, input: Self::Input, params: SystemParamItem<Self::Params>) -> Self::Output {
+    fn run(&self, input: Self::Input, params: Self::Params) -> Self::Output {
         fn call_inner<I, O, P>(f: impl Fn(I, P) -> O, i: I, p: P) -> O {
             f(i, p)
         }
@@ -91,13 +109,39 @@ where
     }
 }
 
-pub struct FunctionSystem<F, Hint>
+pub struct FunctionSystemDef<F, Hint>
 where
     F: SystemParamFunction<Hint>,
 {
     func: F,
-    param_state: Option<<F::Params as SystemParam>::State>,
     signature: PhantomData<fn() -> Hint>,
+}
+
+impl<F, Hint: 'static> FunctionSystemDef<F, Hint>
+where
+    F: SystemParamFunction<Hint>,
+{
+    pub fn new(func: F) -> Self {
+        Self {
+            func,
+            signature: PhantomData
+        }
+    }
+
+    pub fn into_system(self, world: &World) -> impl System<Input = F::Input, Output = F::Output> {
+        FunctionSystem {
+            func: self,
+            param_state: <F::Params as SystemParam>::init_state(world)
+        }
+    }
+}
+
+pub struct FunctionSystem<F, Hint>
+where
+    F: SystemParamFunction<Hint>,
+{
+    func: FunctionSystemDef<F, Hint>,
+    param_state: <F::Params as SystemParam>::State,
 }
 
 impl<F, Hint: 'static> System for FunctionSystem<F, Hint>
@@ -109,11 +153,42 @@ where
 
     fn run(&mut self, input: Self::Input, world: &World) -> Self::Output {
         let params = unsafe {
-            <F::Params as SystemParam>::get_item(
-                self.param_state.as_mut().expect("No ParamState"),
+            <F::Params as SystemParam>::from_state(
+                &mut self.param_state,
                 world,
             )
         };
-        self.func.run(input, params)
+        self.func.func.run(input, params)
     }
+}
+
+#[test]
+fn system_check() {
+
+    struct DummyParam(i32);
+
+    impl SystemParam for DummyParam {
+        type State = i32;
+
+        fn init_state(_world: &World) -> Self::State {
+            2
+        }
+
+        unsafe fn from_state<'world, 'state>(
+            state: &'state mut Self::State,
+            _world: &'world World,
+        ) -> Self {
+            Self(*state)
+        }
+    }
+
+    fn dummy_system(dummy: DummyParam) {
+        assert_eq!(dummy.0, 2);
+    }
+
+    let world = World::new();
+    
+    let mut func_sys = FunctionSystemDef::new(dummy_system).into_system(&world);
+
+    func_sys.run((), &world);
 }
